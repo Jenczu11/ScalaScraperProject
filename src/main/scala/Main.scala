ea163bb9f16bbf520fe529f5c789e4c41b5fdcf7
Bartłomiej Jencz <bartek.jencz@wp.pl>
2021-04-06 14:05:58 +0200

    Simple usage for title search

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,17 @@
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
 
 object Main extends App {
   println("Hello")
   val browser = JsoupBrowser()
-//  val doc = browser.parseFile("src/test/resources/example.html")
-  val doc2 = browser.get("http://example.com")
-  println(doc2)
+  val doc = browser.get("https://www.filmweb.pl/films/search?q=wonder%20woman")
+  val searchResult = doc >> elementList("#searchResult > li")
+  searchResult.foreach(res => {
+    val title = res >?> text(".filmPreview__titleDetails")
+    println(title)
+  })
+
+
 }
