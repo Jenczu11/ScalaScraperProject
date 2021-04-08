760dd10e1ec8abe9860e8b9a1139e95928d6d59a
Mateusz Walczak <mateusz.walczak@p.lodz.pl>
2021-04-08 10:30:09 +0200

    Beginning of parser implementation.

---
+++ src/main/scala/Parser/ParserHelper.scala
@@ -0,0 +1,9 @@
+package Parser
+
+import net.ruippeixotog.scalascraper.model.Element
+
+class ParserHelper {
+  def getItemProp(element: Element, itemPropName: String): String = {
+    "cokolwiek"
+  }
+}
